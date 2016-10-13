b3201075f2a28956b0b268413ae7ceb34d0dff98
Maxim Fedorov <m.fedorov@thenewmotion.com>
2016-10-13 17:06:09 +0200

    Promote blank tile over 'A'

--- scala-rich-model/src/main/scala/Tile.scala
+++ scala-rich-model/src/main/scala/Tile.scala
@@ -3,10 +3,18 @@ package scrabble
 import util.Random
 
 
-case class Tile private(letter: Char) extends AnyVal
+class Tile private(val letter: Char) extends AnyVal
 
 object Tile {
-  private val all = ('A' to 'Z').map(Tile(_))
+  private val blank = new Tile(' ')
+  private val all = ('A' to 'Z').map(new Tile(_)).toSet + blank
+
+  def apply(letter: Char) =
+    all.find(_.letter == letter).getOrElse(throw new IllegalTileLetter(letter))
 
   implicit val ordering = Ordering.fromLessThan[Tile]((a, b) => a.letter < b.letter)
+
+  class IllegalTileLetter(letter: Char) extends Exception(
+    s"Found '$letter', but only 'A' to 'Z' and ' '(blank) tiles can be used in scrabble"
+  )
 }
--- scala-rich-model/src/test/scala/GameStartSpec.scala
+++ scala-rich-model/src/test/scala/GameStartSpec.scala
@@ -9,31 +9,38 @@ class GameStartSpec extends Specification with Tables {
 
   val invitation = Invitation(John).join(Jack)
 
+  def bag(tiles: Char*) = new DeterministicBag(tiles.map(Tile(_)): _*)
+
   def is = s2"""
-
     Player who draws the 'A' tile begins the game ${
-                                           "Bag" | "Expected turns" |>
-      new DeterministicBag(Tile('A'), Tile('C')) !  Seq(John, Jack) |
+              "Bag" | "Expected turns" |>
+      bag('A', 'C') !  Seq(John, Jack) |
       {
         (bag, expectedTurns) =>
           Game.order(invitation, bag) must_== expectedTurns
       }
     }
-
     Or the closest tile to 'A' ${
-                                           "Bag" | "Expected turns" |>
-      new DeterministicBag(Tile('D'), Tile('J')) !  Seq(John, Jack) |
-      new DeterministicBag(Tile('K'), Tile('I')) !  Seq(Jack, John) |
-      new DeterministicBag(Tile('Z'), Tile('X')) !  Seq(Jack, John) |
+              "Bag" | "Expected turns" |>
+      bag('D', 'J') !  Seq(John, Jack) |
+      bag('K', 'I') !  Seq(Jack, John) |
+      bag('Z', 'X') !  Seq(Jack, John) |
       {
         (bag, expectedTurns) =>
           Game.order(invitation, bag) must_== expectedTurns
       }
     }
-
+    But blank tile wins the start of the game ${
+              "Bag" | "Expected turns" |>
+      bag('A', ' ') !  Seq(Jack, John) |
+      {
+        (bag, expectedTurns) =>
+          Game.order(invitation, bag) must_== expectedTurns
+      }
+    }
     First one to draw the same tile wins over follower ${
-                                             "Bag" | "Expected turns" |>
-        new DeterministicBag(Tile('B'), Tile('B')) !  Seq(John, Jack) |
+                "Bag" | "Expected turns" |>
+        bag('B', 'B') !  Seq(John, Jack) |
       {
         (bag, expectedTurns) =>
           Game.order(invitation, bag) must_== expectedTurns
