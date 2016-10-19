4a31b616a1d5260386409b26a80c214381800be6
Maxim Fedorov <m.fedorov@thenewmotion.com>
2016-10-19 22:03:19 +0200

    Add convenient method to draw several tiles from bag

--- scala-rich-model/src/main/scala/bag/Bag.scala
+++ scala-rich-model/src/main/scala/bag/Bag.scala
@@ -9,13 +9,22 @@ trait Bag {
   protected def shuffle(tiles: Tiles): Tiles
   protected def cons(tiles: Tiles): Bag
 
-  def take = {
+  def take: (Tile, Bag) = {
     shuffle(tiles) match {
       case Nil => throw new NoTilesLeft
       case h :: Nil => (h, EmptyBag)
       case h :: ts  => (h, cons(ts))
     }
   }
+
+  def take(n: Int): (Tiles, Bag) = {
+    (1 to n).foldLeft[(Tiles, Bag)]((Nil, this)) {
+      case ((ts, b), _) =>
+        val (t, bb) = b.take
+        (t :: ts, bb)
+    }
+  }
+
 
   override def toString =
     "Bag("+tiles.map(_.letter).mkString(",")+")"
